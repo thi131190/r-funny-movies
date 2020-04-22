3ad542b381e40f1aff07f68142a2ba13a13a65ee
thi1311 <thi.nguyen@coraledge.dev>
2020-04-22 18:42:57 +0700

    add NavBar

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MovieIcon from "@material-ui/icons/Movie";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: 100,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function ButtonAppBar(props) {
+  const classes = useStyles();
+
+  const login = () => {};
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MovieIcon className={classes.icon} />
+            Funny Movies
+          </IconButton>
+          <Typography variant="h6" className={classes.title}></Typography>
+          <Button color="inherit" onClick={login}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
