fb0ba75976e35fb3dc7da61a76b66d5bdbfe7b40
thi1311 <thi.nguyen@coraledge.dev>
2020-04-22 19:31:33 +0700

    add Login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./components/Navbar";
+import Login from "./pages/Login"
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Login />
     </div>
   );
 }
